587858c00a8bcaeedfdef86ac4409159163e7165
김대엽 <kdaeyoub@gmail.com>
2024-08-31 13:26:31 +0900

    gg

--- srs/js/branch.js
+++ srs/js/branch.js
@@ -1,32 +1,51 @@
 export class Branch {
-    constructor(startX, startY, endX, endY, lineWidth) {
-        this.startX = startX;
-        this.startY = startY;
-        this.endX = endX;
-        this.endY = endY;
-        this.color = '#000';
-        this.lineWidth = lineWidth;
+  constructor(startX, startY, endX, endY, lineWidth) {
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+    this.color = '#000';
+    this.lineWidth = lineWidth;
+    this.frame = 60
+    this.gapX = (endX - startX) / this.frame
+    this.gapY = (endY - startY) / this.frame
+    this.currentFrame = 0
+  }
+
+  draw(context) {
+    if (this.currentFrame == this.frame) {
+      console.log('hit')
+      return true;
+    }
+    context.beginPath();
+
+    this.startX += this.gapX 
+    this.startY += this.gapY 
+
+    this.endX = this.startX + this.gapX
+    this.endY = this.startY + this.gapY
+
+
+    context.moveTo(this.startX, this.startY);
+    context.lineTo(this.endX, this.endY);
+    if (this.lineWidth < 3) {
+      context.lineWidth = 0.9;
+    } else if (this.lineWidth < 7) {
+      context.lineWidth = this.lineWidth * 0.7;
+    } else if (this.lineWidth < 10) {
+      context.lineWidth = this.lineWidth * 0.8;
+    } else {
+      context.lineWidth = this.lineWidth * 0.9;
     }
 
-    draw(context) {
-        context.beginPath();
-        context.moveTo(this.startX, this.startY);
-        context.lineTo(this.endX, this.endY);
-        if (this.lineWidth < 3) {
-            context.lineWidth = 0.9;
-          } else if (this.lineWidth < 7) {
-            context.lineWidth = this.lineWidth * 0.7;
-          } else if (this.lineWidth < 10) {
-            context.lineWidth = this.lineWidth * 0.8;
-          } else {
-            context.lineWidth = this.lineWidth * 0.9;
-          }       
+    context.fillStyle = this.color;
+    context.strokeStyle = this.color;
 
-        context.fillStyle = this.color;
-        context.strokeStyle = this.color;
+    context.stroke();
+    context.closePath();
 
-        context.stroke();
-        context.closePath();
+    this.currentFrame++
 
-    }
+    return false
+  }
 }
--- srs/js/tree.js
+++ srs/js/tree.js
@@ -8,7 +8,7 @@ export class Tree {
         this.posX = posX;
         this.posY = posY;
         this.branches = [];
-        this.depth = 11;
+        this.depth = 3;
 
 
         this.init();
@@ -27,16 +27,37 @@ export class Tree {
         const endX = startX + this.cos(angle) * len * (this.depth - depth);
         const endY = startY + this.sin(angle) * len * (this.depth - depth);
 
-        this.branches.push(new Branch(startX, startY, endX, endY, this.depth - depth));
+        if(this.branches[depth] == null){
+            this.branches[depth] = []
+        }
+        this.branches[depth].push(new Branch(startX, startY, endX, endY, this.depth - depth));
 
         this.createBranches(endX, endY, angle - this.random(15, 23), depth + 1);
         this.createBranches(endX, endY, angle + this.random(15, 23), depth + 1);
     }
 
-    draw(context) {
-        for (let i = 0; i < this.branches.length; i++) {
-            this.branches[i].draw(context);
+    draw() {
+        let isEnd = false
+        for (let d = 0; d < this.depth; d++) {
+            // for (let i = 0; i < this.branches[d].length; i++) {
+            for (let i = 0; !isEnd; i++) {
+                console.log(d,i)
+                console.log('1',this.branches)
+                console.log('2',this.branches[d])
+                console.log('3',this.branches[d][i])
+                isEnd = this.branches[d][i].draw(this.context);
+            }
         }
+        // let i = 0
+        // this.branches.forEach(branch => {
+        //     console.log('i', i++)
+        //     isEnd = branch.draw(this.context)
+        // })
+        if (!isEnd) {
+            // requestAnimationFrame(this.draw.bind(this))
+        }
+        // requestAnimationFrame(this.draw.bind(this))
+        console.log('end')
     }
 
     cos(angle) {
